432b1f07cd61e8341451e185d26142570a141624
Clark Farriss <clarkfarriss@Clarks-MacBook-Pro.local>
2019-03-31 19:46:19 -0400

    made responsive

--- public/logic.js
+++ public/logic.js
@@ -1,3 +1,20 @@
+
+
+$('.nav-toggle').click(function(e) {
+  
+  e.preventDefault();
+  $("html").toggleClass("openNav");
+  $(".nav-toggle").toggleClass("active");
+
+});
+
+
+
+
+
+
+
+
 $('.canvas-toggle').hide();
 
  function sideNav() {
